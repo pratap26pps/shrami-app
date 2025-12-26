5f94d05697f2fc870e11e1d7262a03c6e8c9a9eb
pratap26pps <pankajpatna10321@gmail.com>
2025-12-26 14:36:51 +0530

    tailwind configure

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -6,17 +6,17 @@ export default function LoginScreen() {
   const { login } = useContext(AuthContext);
 
   return (
-    <View>
-      <Text>Login Screen</Text>
-      <Text>Login Screen</Text>
-      <Text>Login Screen</Text>
-      <Text>Login Screen</Text>
-      <Text>Login Screen</Text>
-      <Text>Login Screen</Text>
-      <Text>Pankaj</Text>
-      <Text>Jivesh</Text>
-      <Text>Mehar </Text>
-      
+    <View className="flex-1 bg-gray-100 justify-center px-5">
+      <Text className="text-2xl font-bold text-center mb-6">
+        Login Screen
+      </Text>
+
+      <View className="items-center mb-8">
+        <Text className="text-lg">Pankaj</Text>
+        <Text className="text-lg">Jivesh</Text>
+        <Text className="text-lg text-green-800">Mehar</Text>
+      </View>
+
       <Button
         title="Login"
         onPress={() => login({ id: 1, name: "Shrami User" })}
